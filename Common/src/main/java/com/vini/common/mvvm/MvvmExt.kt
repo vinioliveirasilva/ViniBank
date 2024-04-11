260491f483ac8868768be99ebf4f11cb24f10d49
vinioliveirasilva <vinioliveirasilva@hotmail.com>
2024-04-10 22:29:26 -0300

    [Login] - remove funcao nao utilizada

--- Common/src/main/java/com/vini/common/mvvm/MvvmExt.kt
+++ Common/src/main/java/com/vini/common/mvvm/MvvmExt.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
@@ -21,14 +20,6 @@ fun <T> LifecycleOwner.observe(flow: Flow<T>, action: (t: T) -> Unit) {
 
 fun <T, V : ViewModel> Channel<T>.sendInScope(scope: V, vararg event: T) {
     scope.viewModelScope.launch {
-        event.forEach {
-            this@sendInScope.send(it)
-        }
-    }
-}
-
-fun <T, V : CoroutineScope> Channel<T>.sendInScope(scope: V?, vararg event: T) {
-    scope?.launch {
         event.forEach {
             this@sendInScope.send(it)
         }
