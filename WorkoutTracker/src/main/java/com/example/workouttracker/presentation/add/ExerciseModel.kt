63ef5950af570a72c039b607969d94a81bc8fb1a
Vinicius Oliveira <vinioliveirasilva@hotmail.com>
2025-06-05 16:28:49 -0300

    Remove projeto academia

--- WorkoutTracker/src/main/java/com/example/workouttracker/presentation/add/ExerciseModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.workouttracker.presentation.add
-
-data class ExerciseModel(
-    val id: Int,  // Unique identifier
-    val name: String,
-    val muscleGroup: List<String>,
-    val equipment: String,
-    val description: String
-)
--- WorkoutTracker/src/main/java/com/example/workouttracker/presentation/run/UserExerciseModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.workouttracker.presentation.run
-
-import com.example.workouttracker.presentation.add.ExerciseModel
-
-data class UserExerciseModel(
-    val exerciseId: Int,
-    val exercises: List<ExerciseModel>
-)
-
